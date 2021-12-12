3c44de2980d8b2743f5793d97f896c920060fcec
zeprincelini <princelini4@gmail.com>
2021-12-12 15:54:09 +0100

    dashboard cards added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  name = localStorage.getItem('user');
+  name = JSON.parse(localStorage.getItem('user') || '{}');
   constructor() {}
 
   ngOnInit(): void {}
